7fcb03920e26140ab657d57706d460188d6305b8
JuanD HernandezO <juand.hernandezo@autonoma.edu.co>
2026-05-21 11:52:25 -0500

    Documentacion de SaveManager

--- src/main/java/controller/SaveManager.java
+++ src/main/java/controller/SaveManager.java
@@ -4,9 +4,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+/**
+ * Clase que proporciona metodos estaticos para almacenar
+ * el historial de los jugadores y sus respectivos puntajes
+ * en un archivo txt
+ *
+ * @author Diego, Pablo, Jhoem
+ * @version 1.0
+ */
 public class SaveManager {
 
-    // guardar nombre
+    /**
+     * Guarda el nombre de un jugador en el archivo de historial txt
+     * Hace append para no borrar los registros hechos previamente
+     *
+     * @param nombre nombre ingresado por el usuario en la interfaz
+     */
     public static void guardarNombre(String nombre) {
         try (PrintWriter writer = new PrintWriter(new FileWriter("jugador.txt", true))) {
             writer.println("Nombre: " + nombre);
@@ -15,7 +28,12 @@ public class SaveManager {
         }
     }
 
-    //guardar score
+    /**
+     * Guarda el puntaje obtenido por un jugador en el archivo txt
+     * Usa append para registrar la puntuacion al final del archivo
+     *
+     * @param score puntuacion final acumulada por el jugador durante la partida
+     */
     public static void guardarScore(int score) {
         try (PrintWriter writer = new PrintWriter(new FileWriter("jugador.txt", true))) {
             writer.println("Score: " + score);

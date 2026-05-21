f1bc37a674c392890e07363b16092481aab7bf1c
JuanPFrancoL <juanp.franco@autonoma.edu.co>
2026-05-21 09:59:55 -0500

    Pasando cambios a jhoem

--- src/main/java/view/MainFrame.java
+++ src/main/java/view/MainFrame.java
@@ -1,5 +1,7 @@
 package view;
 
+import controller.GameController;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,6 +11,8 @@ public class MainFrame extends JFrame {
     private CardLayout cardLayout;
     private String nombreJugador;
     private PanelGameOver panelGameOver;
+    private Thread gameThread;
+    private GameController controller;
 
     public MainFrame() {
         setTitle("Crossy Santander");
@@ -39,9 +43,17 @@ public class MainFrame extends JFrame {
 
         // Arranca el hilo solo cuando empieza el juego
         if (nombre.equals("juego")) {
+
             gamePanel.requestFocusInWindow();
-            controller.GameController controller = new controller.GameController(gamePanel);
-            new Thread(controller).start();
+
+            if (gameThread == null || !gameThread.isAlive()) {
+
+                controller = new GameController(gamePanel);
+
+                gameThread = new Thread(controller);
+
+                gameThread.start();
+            }
         }
     }
 
